2ee07d56701c8ccad200156ba1aa6304719a1fac
mircoborot <antonio.curci.it@gmail.com>
2016-02-25 00:14:39 +0100

    Added files via upload

---
+++ test_joystick_v01r01/joystick.ino
@@ -0,0 +1,34 @@
+/* ---------------------------------------------------------
+Package     :  wsn v.1
+Module      :  joystick
+Copyright   :  (c) 2016 by Antonio Curci 
+
+Copyright 2014-2016 by Antonio Curci. All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are not permitted.
+-------------------------------------------------------- */
+
+// ******************** WARNINGS
+
+// ******************** LIBRARIES
+ 
+// ******************** DEFINES
+
+// ******************** CONSTANTS
+// PIN
+const byte 
+  PIN_X=A0,
+  PIN_Y=A1,
+  PIN_S=8;    // use PULLUP resistor (inverted logic)
+
+// ******************** VARIABLES
+boolean jen;    // enabled
+
+// ******************** FUNCTIONS
+void joysetup() {pinMode(PIN_S,INPUT_PULLUP);joyen(true);}
+void joyen(bool f) {jen=f;}  
+bool joyena()  {return jen;}  
+byte joyinx() {return analogRead(PIN_X)/10.23;}
+byte joyiny() {return analogRead(PIN_Y)/10.23;}
+bool joyins() {return !digitalRead(PIN_S);}
+
---
+++ test_joystick_v01r01/test_joystick_v01r01.ino
@@ -0,0 +1,37 @@
+/* ---------------------------------------------------------
+Package     :  wsn v.1
+Module      :  main
+Copyright   :  (c) 2016 by Antonio Curci 
+
+Copyright 2014-2016 by Antonio Curci. All rights reserved.
+Redistribution and use in source and binary forms,
+with or without modification, are not permitted.
+
+
+-------------------------------------------------------- */
+// ******************** DEFINES
+#define sersetup()  Serial.begin(9600)
+#define pr(x)  Serial.print(x)
+#define prln(x)  Serial.println(x)
+#define prf(x)  Serial.print(F(x))
+#define prfln(x)  Serial.println(F(x))
+
+// ******************** WARNINGS
+// ******************** CONSTANTS
+// ******************** VARIABLES
+// ******************** FUNCTIONS
+
+// ******************** ARDUINO
+void setup() {
+  sersetup();
+  joysetup();
+  prf("\r\ntest joystick v.01.01 (C) Copyright 2016 by Antonio Curci\r\n");
+}
+
+void loop() {
+  prf("  x=");pr(joyinx());
+  prf("  y=");pr(joyiny());
+  prf("  s=");pr(joyins());
+  prln();
+  delay(20);
+}
